e5dc062b3bbb9d19a1e358d0477d79fb4ff01270
indar <ijpdesigner@yandex.com>
2022-09-29 15:38:57 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -3,6 +3,23 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = null;
 
+const promiseOutput = async (value) => {
+  let count = 0
+  let nilai1, nilai2
+  nilai1 = await promiseTheaterIXX()
+  nilai2 = await promiseTheaterVGC()
+
+  const data = [...nilai1, ...nilai2]
+
+  data.map((result) => {
+    if (result.hasil === nilai) {
+      count++
+    }
+  })
+
+  return count
+};
+
 module.exports = {
   promiseOutput,
 };
